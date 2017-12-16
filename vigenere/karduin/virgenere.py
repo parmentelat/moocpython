db218ae99262d232dd6fd4baa247fbf15edc31aa
Karduin <34542832+Karduin@users.noreply.github.com>
2017-12-16 15:46:59 +0100

    Version encode decode avec un petit menu

--- vigenere/karduin/virgenere.py
+++ vigenere/karduin/virgenere.py
@@ -1,35 +1,86 @@
+#!/usr/bin/env python3
+
+#!python3
+
+
+#----------------------- Fonctions --------------------------
+
+def menu():
+   print("1. Encoder un message")
+   print("2. Decoder un message")
+   print("3. Quitter")
+
+def donnees():
+    cle = input('Entrez la cle : => ')
+    texte = input('Entrez le texte : => ')
+    return (texte, cle)
+
+def encode(texte, cle):
+    flag = 0
+    temp = ""
+    code = ""
+
+    for car in texte:
+        if flag == len(cle):
+            flag = 0
+
+        if ord(car) not in range(97, 123):
+            temp = temp + car
+
+        else:
+            temp = temp + cle[flag]
+        flag += 1
+
+    for pos, car in enumerate(texte):
+        if ord(car) not in range(97, 122):
+            code = code + car
+
+        else:
+            code = code + chr((((ord(car)-97) + (ord(temp[pos])-97))% 26) + 97)
+
+    return code
+
+def decode(texte, cle):
+    flag = 0
+    temp = ""
+    code = ""
+
+    for car in texte:
+        if flag == len(cle):
+            flag = 0
+
+        if ord(car) not in range(97, 123):
+            temp = temp + car
+
+        else:
+            temp = temp + cle[flag]
+        flag += 1
+
+    for pos, car in enumerate(texte):
+        if ord(car) not in range(97, 122):
+            code = code + car
+
+        else:
+            code = code + chr((((ord(car)-97) - (ord(temp[pos])-97))% 26) + 97)
+
+    return code
+
 #---------- Main -----------------------
 
 
-cle = "musique"
-flag = 0
-temp =""
-texte = "j'adore ecouter la radio toute la journee"
-code = ""
+while True:
+    menu()
+    choix = int(input("Votre choix : => "))
 
-for car in texte:
-    if flag == len(cle):
-        flag = 0
+    if choix == 1:
+        texte, cle = donnees()
+        texte_code = encode(texte, cle)
+        print(texte_code)
 
-    if ord(car) not in range(97, 123):
-        temp = temp + car
+    elif choix == 2:
+        texte, cle = donnees()
+        texte_decode = decode(texte, cle)
+        print(texte_decode)
 
-    else:
-        temp = temp + cle[flag]
-    flag += 1
-
-print (len(temp))
-print (len(texte))
-
-for pos, car in enumerate(texte):
-    if ord(car) not in range(97, 122):
-        code = code + car
-
-    else:
-        code = code + chr((((ord(car)-97) + (ord(temp[pos])-97))% 26) + 97)
-
-
-print(temp)
-print(code)
-print(texte)
-
+    elif choix == 3:
+        break
