25dc860e725cd9d71588ae97130a01b6916121f1
Karduin <34542832+Karduin@users.noreply.github.com>
2017-12-20 15:00:38 +0100

    Refonte totale du projet
    Du coup, j'ai fait sans les regex.
    Les fichiers s'ajoutent à la main, on vérifie si il existent (bon c'est hyper simpliste...) 
    C'est très certainement améliorable mais ça marche !

--- corriges2rst/fichiers/txt_to_rst.py
+++ corriges2rst/fichiers/txt_to_rst.py
@@ -5,17 +5,57 @@
 #  Parse fichiers correction.txt en rst pour generer pdf dan sphinx
 #
 #----------------------- Imports ------------------------------
-import re
+
+import os.path
+
+import shutil
 
 
 #-------------------------- Fonctions -------------------------
 
+def file_to_add():
+    """
+       Liste les fichiers à assembler
+    """
+
+    file_liste = []
+    test = True
+
+    while test:
+
+        fichier = input("Entrez le nom du fichier : > ")
+
+        if os.path.isfile(fichier):
+            file_liste.append(fichier)
+
+        else:
+
+            print(f"Je ne trouve pas '{fichier}' !")
+
+        if input("Un autre fichier ? (O/N) :") in "Nn":
+
+            test = False
+
+    return file_liste
+
+
+def assemble(chaine):
+    """
+       Assemble les fichiers lister par 'file_to_add' et crée un fichier temporaire
+    """
+
+    with open('temp_file.txt', 'w', encoding = 'utf8') as sortie:
+        for entree in ma_liste:
+            with open(entree, 'r', encoding = 'utf8') as en_cours:
+                shutil.copyfileobj(en_cours, sortie)
+
+
 def file_to_string(fichier):
     """
-       Ouvre un fichier en lecture et le stock dans une chaine
+       Ouvre le fichier créé par 'assemble' en lecture et le stock dans une chaine
     """
     try:
-        monFichier = open(fichier, 'r', encoding = 'ansi').read()
+        monFichier = open(fichier, 'r', encoding = 'utf8').read()
     except FileNotFoundError:
         print(f"Je ne trouve pas '{fichier}' !")
     else:
@@ -28,34 +68,64 @@ def sauve_fichier(file_out, chaine):
     with open(file_out, 'w', encoding = 'utf8') as fichier:
         fichier.write(chaine)
 
-def exo_title(chaine):
+
+def to_rst(chaine):
     """
-        Recherche dans la chaine la présence de
-        ##################################################
-        # Titre de l'exercice
-        ##################################################
+       Traite la chaine créé par 'file_to_string'
     """
-    regexp1 = "#(.)*Semaine"  #test pas tres concluant
 
-    matches = re.finditer(regexp1, monFichier, re.S)
+    temp_list = chaine.split("\n")
 
-    for objet in matches:
+    for num_ligne, ligne in enumerate(temp_list):
 
-        print(f"{objet.start()}-{objet.end()} {objet.group(0)}")
+        # Si une ligne contient coding on l'enléve
+        if "coding" in ligne:
+
+            temp_list[num_ligne] = ""
+
+        # Si une ligne contient Corrigés faire un titre de niveau 2 et enlever les # de la ligne precedente et suivante
+        elif "Corrigés" in ligne:
+
+            temp_list[num_ligne] = ligne[2:] + "\n" + "-" * len(ligne[2:])
+            temp_list[num_ligne + 1] = ""
+            temp_list[num_ligne - 1] = ""
+
+        # Si une ligne contient Semaine oter les deux premiers carateres et faire un titre de niveau 3
+        # les lignes suivante sont du code donc ajouter .. code:: ipython3
+        elif "Semaine" in ligne:
+
+            temp_list[num_ligne] = ligne[2:] + "\n" + "~" * len(ligne[2:]) + "\n" + "\n.. code:: ipython3\n"
+
+        # Si une ligne contient plusieurs # mettre une ligne vide
+        elif "#######" in ligne:
+
+            temp_list[num_ligne] = ""
+
+
+        # indenter les autres lignes
+        else:
+
+            temp_list[num_ligne] = "    " + ligne
+
+    return "\n".join(temp_list)
+
 
 
 #--------------------- Main -----------------------------------
 
+ma_liste = file_to_add()
 
-fichier = input("Traiter quel fichier ? ")
+assemble(ma_liste)
 
-monFichier = file_to_string(fichier)
+monFichier = file_to_string('temp_file.txt')
 
-if monFichier != None:
+new_fichier = to_rst(monFichier)
 
-    """ Inserer les traitement ici"""
+mon_titre = "Corrigés\n========\n"
 
-    exo_title(monFichier)
+new_fichier = mon_titre + new_fichier
+
+sauve_fichier('corriges_all.rst', new_fichier)
 
 
 
