8e6095162ab27d7ed95970cc4d07dc48d55ea53d
Jean Gorene <jgorene@users.noreply.github.com>
2018-01-03 10:52:18 +0100

    Ajout d'une exception... opérationnel
    Ajout d'une 1ère exception levée que une extension saisie différente de .txt
    Gestion mieux adaptée et plus cohérente il me semble, pour gérer les lignes à conserver ou à supprimer sur les données tà traiter.
    Il reste néanmoins une tas de choses à faire encore pour bien traiter l'objectif de ce type de script
    A suivre...

--- corriges2rst/fichiers/script-to-rst-class.py
+++ corriges2rst/fichiers/script-to-rst-class.py
@@ -22,6 +22,8 @@ from pathlib import Path
 import re
 import textwrap
 
+########################
+
 class CorrigesToRst:
     """
     Regrouper le contenu de plusieurs fihciers txt en 1 seul fichier rst.
@@ -32,18 +34,6 @@ class CorrigesToRst:
         self.ext = ext
         self.path_dir = Path(__file__).parent
         self.files = sorted([file.name for file in list(path_dir.glob(self.ext))])
-        print(f"""
-    Infos de démarrage (pour test essentiellement;)
---- chemin du dossier actif:
-    {self.path_dir}
---- Fichiers {self.ext} présents:
-    {self.files}
-
---- Méthodes:
-    start.auto(), start.manual()
---- Attributs:
-    ext, path_dir, files
-        """)
         
     def _get_ext(self):
         return self._ext
@@ -72,13 +62,15 @@ class CorrigesToRst:
         if self._testExt():
             files_selected = [file for file in self.files if self._test(file)]
             self._launch(files_selected)
-        
-    def _testExt(self):
-        test = re.match(r"[a-z]{3}", self.ext[-3:])
+
+    def _testExt(self): # à optimiser...
+        test = re.search(r"\.[a-z]{3}", self.ext[-4:])
         if  test:
+            if not re.match(r"\.txt$", self.ext[-4:]):
+                raise ExtensionError('Le script ne traite que les .txt ...')
             return True
         else:
-            print(f"test : {self.ext[-3:]}")
+            print(f"test : {self.ext[:]}")
             print(f"Oups ! L'extension doit avoir 3 caractères minuscules...")
             return False
             
@@ -103,53 +95,56 @@ class CorrigesToRst:
             self._read(files_selected)
 
     def _read(self, files_selected):
-        # le liste vide qui va servir à regrouper les données des fichiers txt
+        # liste vide pour stocker toutes les données avant le fichier rst final
         txt_fullcontents = []
         # lecture, regroupement et mise en forme des données pour Sphinx
-        for titre_semaine in files_selected:
+        for semaine in files_selected:
             # gestionnaire de contexte en lecture
-            with open(titre_semaine, encoding='utf-8') as entree:
-                week = re.search(r"(?<=[wW])(\d*)(?=\.)", titre_semaine)[0]
-                self._prepareData(entree, txt_fullcontents, week)
+            with open(semaine, encoding='utf-8') as entree:
+                week = re.search(r"(?<=[wW])(\d*)(?=\.)", semaine)[0]
+                titre2 = f"Corrigés de la semaine {week}"
+                titre2 = "\n\n" + titre2 + "\n" + ("-" * len(titre2)) + "\n"
+                
+                # lecture par ligne... test économie mémoire à confirmer ?
+                txt_contents = ([self._prepareData(line) for line in 
+                                 entree.readlines()])
     
+            # ajout du titre 2 pour chaque fichier sous condition
+            test_re = r"\w*(Semaine \d Séquence \d\n)"
+            if re.findall(test_re, "".join(txt_contents)):
+                txt_contents.insert(0, titre2)
+            else:
+                # si aucune séquence signalée dans un fichier (semaine)
+                txt_contents.insert(0, titre2 + "\n.. code:: ipython3" + 
+                                    "\n\n")
+            
+            # regroupement de tous les corrigés dans un seul objet
+            txt_fullcontents.append("".join(txt_contents))        
+        
         # lance l'écriture dans le fichier de regroupement rst
         self._write("".join(txt_fullcontents), files_selected)
         
-    def _prepareData(self, entree, txt_fullcontents, week):
-    #print(week)
-        for line in entree:
-            #suppression de lignes avec #### et autres... à optimiser ?
-            line = re.sub(r"^#*[\n]", "\n", line)
-            line = re.sub(r"(\# -\*- coding: utf-8 -\*-)", '', line) 
-            line = re.sub(r"^(#*\s{1})$", '', line)
-    
-            # titre 2
-            if re.search(r"\w*(Corrigés de la semaine \d)\n", line):
-                titre2 = re.search(r"\w*(Corrigés de la semaine \d)\n", line)[0]
-                line = "\n\n" + titre2 + ("-" * len(titre2.strip())) + "\n\n"
+    def _prepareData(self, line):
+        # suppression de lignes contenant certains pattern... à optimiser ou
+        # revoir avec une fonction pour gérer les patterns dynamiquement...
+        line = re.sub(r"^#*[\n]", '', line)
+        line = re.sub(r"(\# -\*- coding: utf-8 -\*-)", '', line) 
+        line = re.sub(r"\w*(Corrigés de la semaine \d)\n", '', line)
+        line = re.sub(r"(#!/usr/bin/env python3)", '', line)
+        line = re.sub(r"^(#*\s{1})$", '', line)
             
-            elif re.search(r"(#!/usr/bin/env python3)", line):
-                line = re.sub(r"(#!/usr/bin/env python3)", '', line)
-                titre2 = f"Corrigés de la semaine {week}\n"
-                line = ("\n\n" + titre2 + ("-" * len(titre2.strip())) + "\n\n"
-                        + ".. code:: ipython3" + "\n\n")
-                #print(line)
-    
-            # titre 3
-            elif re.search(r"\w*(Semaine \d Séquence \d\n)", line):
-                titre3 = re.sub(r"^(#{1}\s{1})", '', line)
-                line = ("\n" + titre3 + ("~" * len(titre3.strip())) + "\n\n" 
-                        + ".. code:: ipython3" + "\n\n")
-    
-            # indentation de 4 espaces pour les lignes "bloc de code" 
-            # qui suivent les titres 3
-            else:
-                line = textwrap.indent(line,'    ')
-                
-            # regroupement de tous les corrigés dans un seul objet
-            txt_fullcontents.append(f"{line}")
-    
-        return txt_fullcontents
+        # titre 3
+        if re.search(r"\w*(Semaine \d Séquence \d\n)", line):
+            titre3 = re.sub(r"^(#{1}\s{1})", '', line)
+            line = ("\n" + titre3 + ("~" * len(titre3.strip())) + "\n\n" 
+                    + ".. code:: ipython3" + "\n\n")
+            
+        # indentation de 4 espaces pour les lignes "bloc de code" 
+        # qui suivent les titres 3
+        else:
+            line = textwrap.indent(line,'    ')
+
+        return line
     
     def _write(self, txt_fullcontents, files_selected):
         titre = "Corrigés"
@@ -160,16 +155,38 @@ class CorrigesToRst:
             sortie.write(f"{txt_fullcontents}")
             with open("index.rst", "w", encoding='utf-8') as index:
                 titre_index = 'MOOC Python 3 - corrigés'
-                titre_index = titre_index + "\n" + ("=" * len(titre_index) + "\n" * 2)
-                contents_index = titre_index + ".. toctree::\n   :maxdepth: 2\n\n   corriges"
+                titre_index = (titre_index + "\n" + ("=" * len(titre_index) + 
+                                                     "\n" * 2))
+                contents_index = (
+                        titre_index +
+                        ".. toctree::\n   :maxdepth: 2\n\n   corriges"
+                        )
                 index.write(contents_index)
                 print(f"""
 ... Fichiers txt traités regroupés avec succès dans "corriges.rst" :
     {files_selected}
 ... Fichier index.rst créé ou mis à jour
             """)
-                
+ 
+class ExtensionError(Exception):
+    pass              
+
 start = CorrigesToRst("txt")
 
-#start.auto()
+print(f"""
+    Infos de démarrage (pour test essentiellement)
+--- start instance actuelle de CorrigesToRst("txt")
+--- chemin du dossier actif:
+    start.path_dir = {start.path_dir}
+--- Fichiers {start.ext} présents:
+    start.files = {start.files}
+
+--- Méthodes:
+    auto(), manual()
+--- Attributs:
+    ext, path_dir, files
+        """)
+    
+#c.auto()
 start.manual()
+
