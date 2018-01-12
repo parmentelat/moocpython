7b7009ca7cdcb1f35f7f4ecfe5932036c5a3e9c1
Jean Gorene <jgorene@users.noreply.github.com>
2018-01-12 08:16:55 +0100

    Script généré par Sphinx... restant à optimiser
    Fichier généré par Sphinx dans le dossier du projet au lancement de la commande "sphinx-quickstart".
    Ce fichier permet ensuite de personnaliser les différents supports générés par Sphinx en sortie : html, epub, pdf, etc.
    Ce n'est qu'une 1ère approche et on peut aller plus loin dans la personnalisation...

--- corriges2rst/fichiers/conf.py
+++ corriges2rst/fichiers/conf.py
@@ -11,15 +11,8 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-# import sys
-# import os
-
-
-# sys.path.append(os.path.abspath('..'))
-# sys.path.append(os.path.abspath('./demo/'))
-
 # from sphinx_rtd_theme import __version__
-
+# sys.path.append(os.path.abspath('./demo/'))
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -57,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Python3'
-copyright = '2017, Thierry Parmentelat & Arnaud Legout'
+copyright = '2017 : Licence CC BY-NC-ND Thierry Parmentelat & Arnaud Legout'
 author = 'Thierry Parmentelat & Arnaud Legout'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -75,8 +68,6 @@ release = '1.0'
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
 language = 'fr'
-# latex_engine = 'pdflatex' # par défaut apparemment
-#'pdflatex', 'xelatex', 'lualatex', 'platex'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -93,10 +84,8 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
+# html_theme = 'alabaster' # default
 html_theme = "sphinx_rtd_theme"
-# html_theme_path = ["_themes", ]
-# html_style = None
-# using_rtd_theme = True
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -104,80 +93,15 @@ html_theme = "sphinx_rtd_theme"
 html_theme_options = {
   'collapse_navigation': False,
   'navigation_depth': 4
-  # 'sticky_navigation': True  # Set to False to disable the sticky nav while scrolling.
-  # 'logo_only': True,  # if we have a html_logo below, this shows /only/ the logo with no title text
-  # 'titles_only': False # If True, it'll remove headers within a page from the sidebar.
 }
-
-# Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ["../.."]
-
-
-# The name for this set of Sphinx documents.  If None, it defaults to
-# "<project> v<release> documentation".
-#html_title = None
-
-# A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo =  "./img/funmooc.png" #"./img/python-logo.png"
+html_logo = "./media/python-logo.png" # "./media/funmooc.png"
 
-# The name of an image file (within the static path) to use as favicon of the
-# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
-# pixels large.
-#html_favicon = None
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
-
-# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
-# using the given strftime format.
-#html_last_updated_fmt = '%b %d, %Y'
-
-# If true, SmartyPants will be used to convert quotes and dashes to
-# typographically correct entities.
-#html_use_smartypants = True
-
-# Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
-
-# Additional templates that should be rendered to pages, maps page names to
-# template names.
-#html_additional_pages = {}
-
-# If false, no module index is generated.
-#html_domain_indices = True
-
-# If false, no index is generated.
-#html_use_index = True
-
-# If true, the index is split into individual pages for each letter.
-#html_split_index = False
-
-# If true, links to the reST sources are added to the pages.
-# html_show_sourcelink = True
-
-# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-#html_show_sphinx = True
-
-# If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
-
-# If true, an OpenSearch description file will be output, and all pages will
-# contain a <link> tag referring to it.  The value of this option must be the
-# base URL from which the finished HTML is served.
-# html_use_opensearch = ''
-
-# This is the file name suffix for HTML files (e.g. ".xhtml").
-#html_file_suffix = None
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'SphinxRTDthemedemodoc'
-
 
 # -- Options for HTMLHelp output ------------------------------------------
 
@@ -242,18 +166,12 @@ preamble = r'''
   \end{center}
   \end{sffamily}
 \end{titlepage}
-\setcounter{tocdepth}{3}
+\setcounter{tocdepth}{1}
 '''
-#\setcounter{tocdepth}{2}
 
 latex_elements = {
-# Get LaTeX to handle Unicode correctly... à voir ???
-# 'fontenc': r'\usepackage[T2A,TS1,T1]{fontenc}',
-
-# 'fncychap': '\\usepackage[Lenny]{fncychap}',
-'fncychap': '\\usepackage{fncychap}',
-# Babel for french
-'babel': '\\usepackage[french]{babel}',
+# Changement du style par défaut des entête de chapitre
+'fncychap' : '\usepackage[Bjornstrup]{fncychap}',
 
 # The paper size ('letterpaper' or 'a4paper').
 'papersize': 'a4paper',
@@ -266,8 +184,10 @@ latex_elements = {
 
 # No default title
 'maketitle': '',
-# No default toc
-'tableofcontents': '',
+
+# No default toc (sommaire)
+#'tableofcontents': '',
+
 # No blank page
 'extraclassoptions': 'openany',
 }
@@ -302,8 +222,6 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
-
-
 # -- Options for Epub output ----------------------------------------------
 
 # Bibliographic Dublin Core info.
@@ -323,5 +241,3 @@ epub_copyright = copyright
 
 # A list of files that should not be packed into the epub file.
 epub_exclude_files = ['search.html']
-
-
