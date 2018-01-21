08917559677f41e40eb59584902802062971f5fc
parmentelat <thierry.parmentelat@inria.fr>
2018-01-21 11:03:30 +0100

    take the 2 sample grids out in a shared module to avoid duplication

---
+++ sudoku-solver/sources/samples.py
@@ -0,0 +1,21 @@
+good_array = [[None,None,None,2   ,None,None,None,None,None],
+              [None,None,None,None,None,None,None,9   ,1   ],
+              [None,None,7   ,9   ,None,None,8   ,4   ,None],
+              [None,None,None,5   ,6   ,None,7   ,None,None],
+              [None,None,8   ,None,4   ,None,None,None,None],
+              [None,1   ,None,None,None,3   ,None,None,6   ],
+              [3   ,5   ,None,None,None,None,9   ,None,None],
+              [None,2   ,None,None,None,5   ,None,None,None],
+              [None,4   ,None,7   ,None,None,3   ,None,None]]
+
+
+good_array_2 = [[1   ,None,7   ,None,None,None,None,None,9   ],
+                [None,None,None,2   ,None,None,None,None,6   ],
+                [None,None,None,None,4   ,None,8   ,3   ,None],
+                [None,None,None,None,None,5   ,None,None,None],
+                [4   ,None,None,None,7   ,3   ,2   ,None,None],
+                [6   ,None,None,None,2   ,None,9   ,None,8   ],
+                [7   ,3   ,None,None,None,None,None,None,1   ],
+                [None,None,None,7   ,3   ,None,None,None,None],
+                [None,None,9   ,5   ,None,None,None,2   ,None]]
+
